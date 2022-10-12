439885b8b79f5a070cf1fc932182148661ad4da5
BrianEtemesi <etemecci@gmail.com>
2022-10-12 07:15:39 +0000

    A dog is the only thing on earth that loves you more than you love yourself

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -2,20 +2,6 @@
 #include "dog.h"
 
 /**
- * alloc - allocates memory
- * @d: instance
- * Return: pointer to address of allocated memory
- */
-
-void *alloc(struct dog *d)
-{
-	d = malloc(sizeof(struct dog));
-	if (d == NULL)
-		return (NULL);
-	return (d);
-}
-
-/**
  * init_dog - initializes struct dog
  * @d: instance of a dog
  * @name: name of the dog
@@ -25,8 +11,10 @@ void *alloc(struct dog *d)
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d = alloc(d);
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
