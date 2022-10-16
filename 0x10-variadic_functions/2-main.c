efc68c664977102ea09bdec73079c107abfbfe68
BrianEtemesi <etemecci@gmail.com>
2022-10-16 20:20:06 +0000

    One woman's constant is another woman's variable

--- 0x10-variadic_functions/2-main.c
+++ 0x10-variadic_functions/2-main.c
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-    print_strings(", ", 2, NULL, "Django");
+    print_strings(NULL, 3, NULL, "Django", "unchained");
     return (0);
 }
--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -24,7 +24,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		{
 			printf("(nil)");
 
-			if (separator != NULL)
+			if (i < (n - 1) && separator != NULL)
 			{
 				printf("%s", separator);
 			}
@@ -37,7 +37,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		{
 			printf("%s", str);
 
-			if (separator != NULL)
+			if (i < (n - 1) && separator != NULL)
 			{
 				printf("%s", separator);
 			}
