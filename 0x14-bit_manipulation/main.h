7aad0ca7e5b92d083ada2686a3f292dcd0f8e167
BrianEtemesi <etemecci@gmail.com>
2022-11-03 17:01:25 +0000

    update prototype to header file

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -11,5 +11,6 @@ int _putchar(char c);
 void print_binary(unsigned long int n);
 int get_size(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
 
 #endif /* MAIN_H */
