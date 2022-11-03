e611680fe4e096982289e533fc592c29b0e5f8a8
BrianEtemesi <etemecci@gmail.com>
2022-11-03 08:47:51 +0000

    delete power_of function

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * get_bit - returns value of bit at given index
+ * @n: decimal value
+ * @index: index of bit in the binary value
+ * Return: value of bit at index, -1 if failed
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int rem;
+	int i;
+	int *arr;
+
+	arr = malloc(sizeof(int));
+	if (arr = NULL)
+		return (NULL);
+
+
+}
