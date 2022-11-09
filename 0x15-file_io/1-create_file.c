22cbe7e9d4971467816e3ace9b92afad3a74de33
BrianEtemesi <etemecci@gmail.com>
2022-11-09 09:09:45 +0000

    add NULL check for text content

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,23 +18,27 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (fd < 0)
 		return (-1);
-	for (i = 0; text_content[i]; i++)
-		;
-
-	buf = malloc(sizeof(char) * i);
-	if (buf == NULL)
-		return (-1);
-
-	for (j = 0; j < i; j++)
+	if (text_content)
 	{
-		buf[j] = text_content[j];
+		for (i = 0; text_content[i]; i++)
+			;
+
+		buf = malloc(sizeof(char) * i);
+		if (buf == NULL)
+			return (-1);
+
+		for (j = 0; j < i; j++)
+		{
+			buf[j] = text_content[j];
+		}
+
+		wr = write(fd, buf, i);
+		if (wr < 0)
+			return (-1);
+
+		free(buf);
 	}
 
-	wr = write(fd, buf, i);
-	if (wr < 0)
-		return (-1);
-
 	close(fd);
-	free(buf);
 	return (1);
 }
