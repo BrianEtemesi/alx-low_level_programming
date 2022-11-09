c10af39297fa231255f1781c9a0df7d962f99ec7
BrianEtemesi <etemecci@gmail.com>
2022-11-09 05:50:22 +0000

    check for when write does not return expected number of bytes

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -25,7 +25,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (r < 0)
 		return (0);
 	wr = write(STDOUT_FILENO, buf, r);
-	if (wr < 0)
+	if (wr < 0 || wr != r)
 		return (0);
 	return (wr);
 }
