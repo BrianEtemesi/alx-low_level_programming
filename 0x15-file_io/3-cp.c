a93c87bc7991b211e04d195f529e589ece5f2298
BrianEtemesi <etemecci@gmail.com>
2022-11-09 16:25:09 +0000

    set main to print 1 if _cp is successful

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,67 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int res;
+
+	if (argc != 3)
+	{
+		dprintf(2, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	res = _cp(argv[1], argv[2]);
+	printf("-> %i)\n", res);
+	return (0);
+}
+
+/**
+ * _cp - copies content of a file to another
+ * @file_from: source file
+ * @file_to: destination file
+ * Return: 1 on success, 97,98,99,100 on failure
+ */
+
+int _cp(const char *file_from, const char *file_to)
+{
+	ssize_t fd1, fd2, wr, c1, c2;
+	char *buf;
+
+	buf = malloc(sizeof(char) * 1024);
+	if (buf == NULL)
+		dprintf(STDOUT_FILENO, "failed to allocate memory\n");
+	fd1 = open(file_from, O_RDONLY);
+	r = read(fd1, buf, 1024);
+	if (fd1 < 0 || r < 0)
+	{
+		dprintf(STDOUT_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	c1 = close(fd1);
+	if (c1 < 0)
+	{
+		dprintf("Error: Can't close fd %d\n", fd1);
+		exit(100);
+	}
+	fd2 = open(file_to, O_RDWR | O_CREAT | O_TRUNC, 0664);
+	wr = write(fd2, buf, r);
+	if (fd2 < 0 || wr < 0)
+	{
+		dprint(STDOUT_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+	c2 = close(fd2);
+	if (c2 < 0)
+	{
+		dprintf("Error: Can't close fd %d\n", fd2);
+		exit(100);
+	}
+	free(buf);
+	return (1);
+}
