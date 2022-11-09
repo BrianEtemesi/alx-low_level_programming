bdd75ee945fe7408aa9856d927b95745dc94b9ac
BrianEtemesi <etemecci@gmail.com>
2022-11-09 05:41:26 +0000

    Tread lightly, she is near

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,3 +1,28 @@
 #include "main.h"
 
+/**
+ * read_textfile - reads text file and prints it STDOUT
+ * @filename: name of the file
+ * @letters: number of letters to be printed
+ * Return: actual number of letters printed, 0 if fails
+ */
 
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t fd, r, wr;
+	char *buf;
+
+	buf = malloc(sizeof(char) * letters);
+	if (buf == NULL)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+		return (0);
+	r = read(fd, buf, letters);
+	if (r < 0)
+		return (0);
+	wr = write(STDOUT_FILENO, buf, r);
+	if (wr < 0)
+		return (0);
+	return (wr);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -3,5 +3,9 @@
 
 #include <stdio.h>
 #include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
 
 #endif /* MAIN_H */
