b02ea9ea10b30f2c40823ea96b6e0ee702995072
BrianEtemesi <etemecci@gmail.com>
2022-11-09 07:05:04 +0000

    remove check for if wr != r

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -25,7 +25,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (r < 0)
 		return (0);
 	wr = write(STDOUT_FILENO, buf, r);
-	if (wr < 0 || wr != r)
+	if (wr < 0)
 		return (0);
 
 	free(buf);
