831b86e23aefb9a02b6083ce844daeb6d0b0cf70
BrianEtemesi <etemecci@gmail.com>
2022-11-09 08:54:58 +0000

    remove null byte from buffer

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,17 +21,16 @@ int create_file(const char *filename, char *text_content)
 	for (i = 0; text_content[i]; i++)
 		;
 
-	buf = malloc(sizeof(char) * (i + 1));
+	buf = malloc(sizeof(char) * i);
 	if (buf == NULL)
 		return (-1);
 
-	for (j = 0; j <= i; j++)
+	for (j = 0; j < i; j++)
 	{
 		buf[j] = text_content[j];
 	}
-	buf[j] = '\0';
 
-	wr = write(fd, buf, i + 1);
+	wr = write(fd, buf, i);
 	if (wr < 0)
 		return (-1);
 
