8a637845186d5a953c8c4b9ca92872c53e781f01
BrianEtemesi <etemecci@gmail.com>
2022-11-21 14:51:41 +0000

    changed multiple table to use _putchar

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,18 +9,36 @@ void print_times_table(int n)
 {
 	int i, j, k;
 
-	if (n < 15 && n > 0)
+	if (n <= 15 && n >= 0)
 	{
 		for (i = 0; i <= n; i++)
 		{
-			for (j = 0; j <= n; j++)
+			_putchar(48);
+			for (j = 1; j <= n; j++)
 			{
 				k = j * i;
-				printf("%3d", k);
-				if (j != n)
-					putchar(',');
+				_putchar(44);
+				_putchar(32);
+				if (k <= 9)
+				{
+					_putchar(32);
+					_putchar(32);
+					_putchar(k + 48);
+				}
+				else if (k <= 99)
+				{
+					_putchar(32);
+					_putchar((k / 10) + 48);
+					_putchar((k % 10) + 48);
+				}
+				else
+				{
+					_putchar(((k / 100) % 10) + 48);
+					_putchar(((k / 10) % 10) + 48);
+					_putchar((k % 10) + 48);
+				}
 			}
-			putchar('\n');
+			_putchar('\n');
 		}
 	}
 }
