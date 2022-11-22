3b650111db1b8e10ba1c40e20d79e422fa0da235
BrianEtemesi <etemecci@gmail.com>
2022-11-22 07:26:09 +0000

    fizzbuzz correction

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -18,27 +18,21 @@ int main(void)
 		if (i % 15 == 0)
 		{
 			printf("FizzBuzz");
-			printf(" ");
 		}
 		else if (i % 3 == 0)
 		{
 			printf("Fizz");
-			printf(" ");
 		}
 		else if (i % 5 == 0)
 		{
 			printf("Buzz");
-			printf(" ");
-		}
-		else if (i == 100)
-		{
-			printf("%d", i);
 		}
 		else
 		{
 			printf("%d", i);
-			printf(" ");
 		}
+		if (i != 100)
+			printf(" ");
 	}
 	printf("\n");
 	return (0);
