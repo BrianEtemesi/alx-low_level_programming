9c4f3f661263c9a700456054ce70e0f6ee5b4f71
BrianEtemesi <etemecci@gmail.com>
2022-11-22 05:42:19 +0000

    Fibonacci 1

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - prints the first 50 Fibonacci numbers
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i, j, k, n;
+
+	i = 1;
+	j = 2;
+	
+	printf("%d, %d, ", i, j);
+
+	for (n = 0; n < 48; n++)
+	{
+		k = i + j;
+		printf("%d, ", k);
+		i = j;
+		j = k;
+	}
+	putchar('\n');
+	return (0);
+}
