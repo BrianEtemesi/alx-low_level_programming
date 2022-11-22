254aa73889f3a4ac67aafafa5b6b41d1ec4df23a
BrianEtemesi <etemecci@gmail.com>
2022-11-22 17:00:33 +0000

    Great leaders sacrifice numbers to save people

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,12 +8,23 @@
 
 int _atoi(char *s)
 {
-	int i, res = 0;
+	int i, sig, brk, res = 0;
+	sig = -1;
+	brk = 0;
 
-	for (i = 0; s[i]; ++i)
+	for (i = 0; s[i]; i++)
 	{
-		res = res * 10 + s[i] - '0';
+		if (s[i] == '-')
+			sig = sig * -1;
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			res = res * 10;
+			res = res - (s[i] - '0');
+			brk = 1;
+		}
+		else if (brk == 1)
+			break;
 	}
-
+	res = sig * res;
 	return (res);
 }
