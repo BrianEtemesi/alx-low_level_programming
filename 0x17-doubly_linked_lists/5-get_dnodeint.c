eb9eaeb560f56346c32e680e26bd338c1125adc7
BrianEtemesi <etemecci@gmail.com>
2022-12-20 18:21:13 +0000

    add check for out of range index

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -9,10 +9,21 @@
 
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-	unsigned int i = 0;
+	unsigned int j, i = 0;
 	dlistint_t *temp = head;
+	dlistint_t *temp1 = head;
 
 	if (head == NULL)
+		return (NULL);
+
+	j = 0;
+	while (temp1 != NULL)
+	{
+		j++;
+		temp1 = temp1->next;
+	}
+
+	if (index > j)
 		return (NULL);
 
 	while (i < index)
