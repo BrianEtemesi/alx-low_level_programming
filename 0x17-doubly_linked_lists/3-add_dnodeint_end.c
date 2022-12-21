036618d1140162f50945c746355a0cfa4d53589d
BrianEtemesi <etemecci@gmail.com>
2022-12-21 13:23:08 +0000

    move create node function to main

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,25 +1,6 @@
 #include "lists.h"
 
 /**
- * createNode - creates new node
- * @n: data value
- * Return: pointer to newly created node
- */
-
-dlistint_t *createNode(const int n)
-{
-	dlistint_t *newNode = malloc(sizeof(dlistint_t));
-
-	if (newNode == NULL)
-		return (NULL);
-
-	newNode->n = n;
-	newNode->prev = NULL;
-	newNode->next = NULL;
-
-	return (newNode);
-}
-/**
  * add_dnodeint_end - adds node at the end of a linked list
  * @head: pointer to address of head node
  * @n: data value
@@ -28,8 +9,16 @@ dlistint_t *createNode(const int n)
 
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-	dlistint_t *temp = *head;
-	dlistint_t *newNode = createNode(n);
+	dlistint_t *newNode, *temp = *head;
+
+	newNode = malloc(sizeof(dlistint_t));
+	if (newNode == NULL)
+		return (NULL);
+
+	newNode->n = n;
+	newNode->prev = NULL;
+	newNode->next = NULL;
+
 
 	if ((*head) == NULL)
 	{
