fb9a72b4edc94341652120d95dca96673e856958
BrianEtemesi <etemecci@gmail.com>
2022-12-21 10:47:02 +0000

    add function to get length of linked list

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,44 @@
+#include "lists.h"
+
+/**
+ * getlen - gets length of list
+ * @h: pointer to head node
+ * Return: length of node
+ */
+
+size_t getlen(dlistint_t *h)
+{
+	size_t i = 0;
+	
+	if (h == NULL)
+		return (i);
+	while (h != NULL)
+	{
+		i++;
+		h = h->next;
+	}
+
+	return (i);
+}
+/**
+ * insert_dnodeint_at_index - inserts node at specified index
+ * @h: double pointer to head node
+ * @idx: index
+ * @n: data value
+ * Return: address of new node
+ */
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *temp = *h;
+	size_t len = getlen(temp);
+
+	printf("length of list: %ld\n", len);
+	printf("node data: %d\n", n);
+	if (idx > len)
+		return (NULL);
+
+	return (temp);
+
+
+}
