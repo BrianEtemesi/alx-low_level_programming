1c186d079b9972207d1484af231fb217ce848ef4
BrianEtemesi <etemecci@gmail.com>
2023-01-03 08:02:19 +0000

    A library is not a luxury but one of the necessities of life

---
+++ 0x18-dynamic_libraries/0-isupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase characters
+ * @c: character to be checked
+ *
+ * Return: 1 if true, 0 if otherwise
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+	return (0);
+}
---
+++ 0x18-dynamic_libraries/1-isdigit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for a digit
+ * @c: input to be checked
+ *
+ * Return: 0
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+	return (0);
+}
