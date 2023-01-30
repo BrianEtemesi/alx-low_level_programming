d2577f3fdb56e5aafa79f5b636217313accc1de9
BrianEtemesi <etemecci@gmail.com>
2023-01-30 10:10:52 +0000

    make a copy of value

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,6 +12,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int size, index;
 	hash_node_t *newnode, *temp;
+	char *value1 = (char *)value;
+
 	/* check if key is an empty string */
 	if (strlen(key) == 0)
 		exit(0);
@@ -20,7 +22,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (newnode == NULL)
 		exit(0);
 	newnode->key = (char *)key;
-	newnode->value = (char *)value;
+	newnode->value = value1;
 	newnode->next = NULL;
 	/* generate hash key/index. This is based on the array size */
 	size = ht->size;
