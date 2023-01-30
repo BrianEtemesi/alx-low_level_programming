54720481ff049bf8e6d2457ef19095f852a1d7da
BrianEtemesi <etemecci@gmail.com>
2023-01-30 14:37:31 +0000

    assign key using strdup

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -20,7 +20,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	newnode = malloc(sizeof(hash_node_t));
 	if (newnode == NULL)
 		exit(0);
-	newnode->key = (char *)key;
+	newnode->key = strdup(key);
 	newnode->value = strdup(value);
 	newnode->next = NULL;
 	/* generate hash key/index. This is based on the array size */
