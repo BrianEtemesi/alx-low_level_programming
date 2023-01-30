b752a15801d5b5d5d54e80e9226ab8fd8325d4a3
BrianEtemesi <etemecci@gmail.com>
2023-01-30 10:04:10 +0000

    ht['betty'] = 'cool'

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,45 @@
+#include "hash_tables.h"
+/**
+ * hash_table_set - adds element to the hash table
+ * @ht: hash table
+ * @key: key
+ * @value: value associated with the key
+ *
+ * Return: 1 for success, 0 if func fails
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int size, index;
+	hash_node_t *newnode, *temp;
+	/* check if key is an empty string */
+	if (strlen(key) == 0)
+		exit(0);
+	/* create new node and assign key and value */
+	newnode = malloc(sizeof(hash_node_t));
+	if (newnode == NULL)
+		exit(0);
+	newnode->key = (char *)key;
+	newnode->value = (char *)value;
+	newnode->next = NULL;
+	/* generate hash key/index. This is based on the array size */
+	size = ht->size;
+	index = key_index((const unsigned char *)key, size);
+	/* go to index of the array and check for collision */
+	/* if no collision; save key value pair at index */
+	if (ht->array[index] == NULL)
+	{
+		ht->array[index] = newnode;
+	}
+	/* if theres a collision, stack new node at the beginning of the list */
+	else
+	{
+		/* save node at index to temp */
+		temp = ht->array[index];
+		/* assign newnode to index */
+		ht->array[index] = newnode;
+		/* assign temp to newnode next */
+		newnode->next = temp;
+	}
+	return (1);
+}
