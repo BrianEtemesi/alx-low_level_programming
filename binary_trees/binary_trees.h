4df0eb8ade0e6b0ad73dcfdbff35808bb8fc5a4a
BrianEtemesi <etemecci@gmail.com>
2023-03-03 19:33:13 +0000

    New node

--- binary_trees/binary_trees.h
+++ binary_trees/binary_trees.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdlib.h>
+
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -17,6 +19,12 @@ struct binary_tree_s
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
 };
+
+typedef struct binary_tree_s binary_tree_t;
+
+void binary_tree_print(const binary_tree_t *);
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 
 
 #endif /* MAIN_H */
