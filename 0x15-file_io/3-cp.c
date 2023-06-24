fea07fa33b19f67046815e1aade8593fd6f4480c
BrianEtemesi <etemecci@gmail.com>
2023-06-24 09:13:59 +0000

    copy contents of file to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,67 +1,73 @@
 #include "main.h"
+#include <stdio.h>
+
+/**
+ * error_file - check if files can be opened.
+ * @file_from: file_from.
+ * @file_to: file_to.
+ * @argv: arguments vector.
+ */
+
+void error_file(int file_from, int file_to, char *argv[])
+{
+	if (file_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	if (file_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+}
 
 /**
  * main - entry point
  * @argc: argument count
- * @argv: argument vector
- * Return: 0
+ * @argv: arguments vector.
+ * Return: 0 on success
  */
 
 int main(int argc, char *argv[])
 {
-	int res;
+	int file_from, file_to, err_close;
+	ssize_t nchars, writecount;
+	char buf[1024];
 
 	if (argc != 3)
 	{
-		dprintf(2, "Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
 		exit(97);
 	}
-	res = _cp(argv[1], argv[2]);
-	printf("-> %i)\n", res);
+
+	file_from = open(argv[1], O_RDONLY);
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+	error_file(file_from, file_to, argv);
+
+	nchars = 1024;
+	while (nchars == 1024)
+	{
+		nchars = read(file_from, buf, 1024);
+		if (nchars == -1)
+			error_file(-1, 0, argv);
+		writecount = write(file_to, buf, nchars);
+		if (writecount == -1)
+			error_file(0, -1, argv);
+	}
+
+	err_close = close(file_from);
+	if (err_close == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
+
+	err_close = close(file_to);
+	if (err_close == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
 	return (0);
-}
-
-/**
- * _cp - copies content of a file to another
- * @file_from: source file
- * @file_to: destination file
- * Return: 1 on success, 97,98,99,100 on failure
- */
-
-int _cp(const char *file_from, const char *file_to)
-{
-	ssize_t fd1, fd2, wr, r, c1, c2;
-	char *buf;
-
-	buf = malloc(sizeof(char) * 1024);
-	if (buf == NULL)
-		dprintf(STDOUT_FILENO, "failed to allocate memory\n");
-	fd1 = open(file_from, O_RDONLY);
-	r = read(fd1, buf, 1024);
-	if (fd1 < 0 || r < 0)
-	{
-		dprintf(STDOUT_FILENO, "Error: Can't read from file %s\n", file_from);
-		exit(98);
-	}
-	c1 = close(fd1);
-	if (c1 < 0)
-	{
-		dprintf(STDOUT_FILENO, "Error: Can't close fd %ld\n", fd1);
-		exit(100);
-	}
-	fd2 = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	wr = write(fd2, buf, r);
-	if (fd2 < 0 || wr < 0)
-	{
-		dprintf(STDOUT_FILENO, "Error: Can't write to %s\n", file_to);
-		exit(99);
-	}
-	c2 = close(fd2);
-	if (c2 < 0)
-	{
-		dprintf(STDOUT_FILENO, "Error: Can't close fd %ld\n", fd2);
-		exit(100);
-	}
-	free(buf);
-	return (1);
 }
